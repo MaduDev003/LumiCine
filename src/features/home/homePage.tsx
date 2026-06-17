5be4119b38f9450e132b7c9b8ae73d66a6cf5c8e
MaduDev003 <mariabevenuto062@gmail.com>
2026-06-17 13:08:35 -0300

    feat: features folder for arquitectchure

---
+++ src/features/home/homePage.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h1 className="text-4xl font-bold text-gray-900 dark:text-white">
+        LumiCine Start
+      </h1>
+    </div>
+  );
+}
