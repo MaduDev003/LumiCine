417c00d3c6e98b3f064dc355fdd2146c746d54cd
MaduDev003 <mariabevenuto062@gmail.com>
2026-06-18 16:04:05 -0300

    feat: component for filter movie by date

---
+++ src/features/home/components/DateMovieFilter.tsx
@@ -0,0 +1,32 @@
+type Props = {
+  selectedDate: boolean;
+  day: string;
+  date: number;
+};
+
+export default function DateMovieFilter({
+  selectedDate,
+  day,
+  date,
+}: Props) {
+  return (
+    <button
+      className={`
+        ${
+          selectedDate
+            ? "bg-accent"
+            : "border border-tertiary-dark hover:bg-accent hover:border-accent transition-colors duration-200"
+        }
+        w-24 h-20 rounded-xl relative cursor-pointer
+      `}
+    >
+      <span className="absolute text-sm top-1.5 left-1/2 -translate-x-1/2">
+        {day}
+      </span>
+
+      <h3 className="h-full text-xl flex items-center justify-center mt-2">
+        {date}
+      </h3>
+    </button>
+  );
+}
--- src/features/home/homePage.tsx
+++ src/features/home/homePage.tsx
@@ -1,15 +1,26 @@
 import Header from "../../components/layout/Header";
+import DateMovieFilter from "./components/DateMovieFilter";
 import AvatarMovie from "../../assets/images/avatar_h_.jpg";
 import { ChevronRight, ChevronLeft } from "lucide-react";
+
+/* TODO: isso será futuramente repensado - no momento é válido para construção do layout e leitura como um todo */
+const dates = [
+  { day: "Seg", date: 29, selectedDate: true },
+  { day: "Ter", date: 30, selectedDate: false },
+  { day: "Qua", date: 1, selectedDate: false },
+  { day: "Qui", date: 2, selectedDate: false },
+  { day: "Sex", date: 3, selectedDate: false },
+];
 
 export default function HomePage() {
   return (
     <>
       <Header />
-      <main className="w-full py-10">
-        <div className="px-7.5 flex justify-center">
-          <div className="w-9/12 max-w-5xl">
-            <div className="aspect-21/9 rounded-2xl relative overflow-hidden shadow-sm">
+
+      <main className="w-full py-8">
+        <div className="px-6 flex justify-center">
+          <div className="w-8/12 max-w-4xl">
+            <div className="aspect-21/9 rounded-xl relative overflow-hidden shadow-sm">
               <img
                 src={AvatarMovie.src}
                 alt="Avatar Movie Poster"
@@ -19,75 +30,35 @@ export default function HomePage() {
               <div className="absolute inset-0 bg-linear-to-r from-black/60 via-black/40 to-transparent" />
             </div>
 
-            <div className="flex mt-4 justify-center h-16 gap-3"> 
-              <span className="bg-font-dark h-2.5 w-10 rounded-full"></span>
-              <span className="bg-tertiary-dark h-2.5 w-10 rounded-full"></span>
-              <span className="bg-tertiary-dark h-2.5 w-10 rounded-full"></span>
-              <span className="bg-tertiary-dark h-2.5 w-10 rounded-full"></span>
+            <div className="flex mt-3 justify-center h-12 gap-2">
+              <span className="bg-font-dark h-2 w-8 rounded-full hover:bg-font-dark transition-colors duration-200"></span>
+              <span className="bg-tertiary-dark h-2 w-8 rounded-full hover:bg-font-dark transition-colors duration-200"></span>
+              <span className="bg-tertiary-dark h-2 w-8 rounded-full hover:bg-font-dark transition-colors duration-200"></span>
+              <span className="bg-tertiary-dark h-2 w-8 rounded-full hover:bg-font-dark transition-colors duration-200"></span>
             </div>
 
-            <section className="bg-secondary-dark mt-4 rounded-2xl h-30 flex items-center justify-between px-10">
+            <section className="bg-secondary-dark mt-3 rounded-xl h-24 flex items-center justify-between px-6">
               <button
                 disabled
                 className="text-font-dark/30 cursor-default"
               >
-                <ChevronLeft className="w-10 h-10 stroke-1" />
+                <ChevronLeft className="w-8 h-8 stroke-1" />
               </button>
-                {/* TODO: Cada botão clicavel será um componente criado nesta pasta de componenets Pois só será usado na HomePage */}
-              <div className="flex overflow-x-clip scrollbar-hide gap-7">
-                <button className="bg-accent w-28 h-24 rounded-2xl relative cursor-pointer">
-                  <span className="absolute text-base top-2 left-1/2 -translate-x-1/2">
-                    Seg
-                  </span>
 
-                  <h3 className="h-full text-2xl flex items-center justify-center mt-2">
-                    29
-                  </h3>
-                </button>
-
-                <button className="border border-tertiary-dark w-28 h-24 rounded-2xl relative cursor-pointer hover:bg-accent hover:border-accent transition-colors duration-200">
-                  <span className="absolute text-base top-2 left-1/2 -translate-x-1/2">
-                    Ter
-                  </span>
-
-                  <h3 className="h-full text-2xl flex items-center justify-center mt-2">
-                    30
-                  </h3>
-                </button>
-
-                <button className="border border-tertiary-dark w-28 h-24 rounded-2xl relative cursor-pointer hover:bg-accent hover:border-accent transition-colors duration-200">
-                  <span className="absolute text-base top-2 left-1/2 -translate-x-1/2">
-                    Qua
-                  </span>
-
-                  <h3 className="h-full text-2xl flex items-center justify-center mt-2">
-                    01
-                  </h3>
-                </button>
-
-                <button className="border border-tertiary-dark w-28 h-24 rounded-2xl relative cursor-pointer hover:bg-accent hover:border-accent transition-colors duration-200">
-                  <span className="absolute text-base top-2 left-1/2 -translate-x-1/2">
-                    Qui
-                  </span>
-
-                  <h3 className="h-full text-2xl flex items-center justify-center mt-2">
-                    02
-                  </h3>
-                </button>
-
-                <button className="border border-tertiary-dark w-28 h-24 rounded-2xl relative cursor-pointer hover:bg-accent hover:border-accent transition-colors duration-200">
-                  <span className="absolute text-base top-2 left-1/2 -translate-x-1/2">
-                    Sex
-                  </span>
-
-                  <h3 className="h-full text-2xl flex items-center justify-center mt-2">
-                    03
-                  </h3>
-                </button>
+              <div className="flex overflow-x-clip scrollbar-hide gap-5">
+                {dates.map((item) => (
+                  <DateMovieFilter
+                    key={`${item.day}-${item.date}`}
+                    selectedDate={item.selectedDate}
+                    day={item.day}
+                    date={item.date}
+                  />
+                ))}
               </div>
+              
 
               <button className="text-font-dark hover:text-white transition-colors duration-200 hover:scale-105 cursor-pointer">
-                <ChevronRight className="w-10 h-10 stroke-1" />
+                <ChevronRight className="w-8 h-8 stroke-1" />
               </button>
             </section>
           </div>
