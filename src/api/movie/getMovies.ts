2acb203ff301e7e161ff8df4241ce8a71cd50de6
MaduDev003 <mariabevenuto062@gmail.com>
2026-06-27 13:49:38 -0300

    fix: indentation

--- src/api/movie/getMovies.ts
+++ src/api/movie/getMovies.ts
@@ -3,8 +3,8 @@ import { endpoints } from "../config/endpoints";
 
 export async function getMovies(page: number) {
     try{
-                const { data } = await httpClient.get(
-        `${endpoints.movie.getMovies}?page=${page}&language=pt-BR`
+        const { data } = await httpClient.get(
+            `${endpoints.movie.getMovies}?page=${page}&language=pt-BR`
         );
         
         return data.results;
