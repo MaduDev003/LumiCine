9ff939d4d40bc03c41de2e62e8808907ac8fb380
MaduDev003 <mariabevenuto062@gmail.com>
2026-07-20 18:24:43 -0300

    feat: product resume at lumibar menu page

--- src/app/lumibar/page.tsx
+++ src/app/lumibar/page.tsx
@@ -1,58 +1,85 @@
-"use client";
+"use client"
 
 import { useState } from "react";
-import { useMovieContext } from "@/src/context/MovieContext";
-import Header from "@/src/components/layout/Header";
-import LumiBar from "@/src/components/layout/Lumibar";
-import Footer from "@/src/components/layout/Footer";
-import ButtonCine from "@/src/components/ui/ButtonCine";
-import MenuListElements from "@/src/components/ui/MenuListElements";
+import { useRouter } from "next/navigation";
 import { X } from "lucide-react";
+import { useMovieContext } from "@/src/context/MovieContext";
+import MenuListElements from "@/src/components/ui/MenuListElements";
+import Header from "@/src/components/layout/Header";
+import Footer from "@/src/components/layout/Footer";
+import LumiBar from "@/src/components/layout/Lumibar";
+import ButtonCine from "@/src/components/ui/ButtonCine";
+import CheckoutProduct from "@/src/features/checkout/components/CheckoutProduct";
+import { useCheckoutStore } from "@/src/store/checkoutStore";
 
-export default function LumiBarPrincipal() {
+export default function CheckoutLumiBar() {
   const [menu, setMenu] = useState(false);
   const { nowPlayingMoviesData, comingSoonMoviesData } = useMovieContext();
+  const lumibar = useCheckoutStore((state) => state.lumibar);
+  const router = useRouter();
 
   return (
     <>
       {menu && (
-          <div className="w-full h-screen flex items-center justify-center relative">
-            
-            <button
-              onClick={() => setMenu(false)}
-              className="absolute top-6 right-6 p-2 rounded-full transition-all hover:bg-white/10 hover:backdrop-blur-sm"
-            >
-              <X className="w-6 h-6 text-font-dark " />
-            </button>
-    
-            <MenuListElements className="flex-col gap-10 items-center" />
-    
-          </div>
-        )}
-        {!menu && (
-             <>
-              <Header
-                setMenu={setMenu}
-                allMoviesForSearch={[
-                  ...nowPlayingMoviesData,
-                  ...comingSoonMoviesData,
-                ]}
-              />
-              <main className="mt-3 mb-12">
-                    <div className="px-3 flex justify-center pt-8 pb-13">
-                      <div className="w-full max-w-275 mx-60 pb-5 flex flex-col gap-4 lg:min-h-200">
-                        <LumiBar />
-                        <div className="flex justify-center items-center ">
-                          <ButtonCine text="Comprar" className="bg-accent w-50 h-10 mt-10"/>
-                        </div>
-                      </div>
-                  </div>
+        <div className="w-full h-screen flex items-center justify-center relative">
+          <button
+            onClick={() => setMenu(false)}
+            className="absolute top-6 right-6 p-2 rounded-full transition-all hover:bg-white/10 hover:backdrop-blur-sm"
+          >
+            <X className="w-6 h-6 text-font-dark" />
+          </button>
+
+          <MenuListElements className="flex-col gap-10 items-center" />
+        </div>
+      )}
+
+      {!menu && (
+        <>
+          <Header
+            setMenu={setMenu}
+            allMoviesForSearch={[
+              ...nowPlayingMoviesData,
+              ...comingSoonMoviesData,
+            ]}
+          />
+
+          <main className="mt-3 md:mb-0 mb-17 lg:mb-40">
+            <div className="px-3 flex justify-center pt-8">
+              <div className="w-full max-w-280 mx-auto pb-0 lg:pb-5 flex flex-col gap-4 lg:min-h-200">
+                <div className="flex flex-col-reverse lg:flex-row gap-8 items-stretch lg:h-220 sm:h-400 lg:mt-4 mt-10">
                   
-              </main>
-              <Footer />
-             </>     
-        )}
-      
+                  <div className="flex-2 gap-7 flex flex-col lg:mt-0 mt-10">
+                    <div className="flex justify-center items-center gap-5">
+                      <LumiBar />
+                    </div>
+
+                    <div className="lg:flex lg:flex-col lg:justify-end lg:items-center h-full mt-10 flex justify-center">
+                      <ButtonCine
+                        onClick={() => router.push("/checkout/payment")}
+                        text="Comprar"
+                        className="
+                          w-50 h-10 text-font-dark
+                          bg-accent
+                          transition-all duration-300
+                          hover:scale-105
+                          cursor-pointer
+                          hover:brightness-110
+                          hover:shadow-[0_25px_60px_rgba(0,0,0,0.2)]
+                        "
+                      />
+                    </div>
+                  </div>
+
+                  {lumibar.length > 0 && <CheckoutProduct />}
+
+                </div>
+              </div>
+            </div>
+          </main>
+
+          <Footer />
+        </>
+      )}
     </>
   );
 }
--- src/features/checkout/components/CheckoutProduct.tsx
+++ src/features/checkout/components/CheckoutProduct.tsx
@@ -1,33 +1,38 @@
-"use client"
+"use client";
+
 import { useState } from "react";
 import { useRouter } from "next/navigation";
-import { useCheckoutStore } from "@/src/store/checkoutStore";
-import { Checkout } from "@/src/types/checkoutType";
 import { Trash2 } from "lucide-react";
+
+import { useCheckoutStore } from "@/src/store/checkoutStore";
 import { sumLumibarProducts } from "@/src/services/checkout/lumiBarService";
-import ButtonCine from "../../../components/ui/ButtonCine";
-import DivisionBar from "@/src/components/ui/DivisorBar";
 import { calcItemsTotalPrice } from "@/src/services/paymentService";
 import { BRFormat } from "@/src/utils/fixValueForBrazilianFormat";
 
+import ButtonCine from "../../../components/ui/ButtonCine";
+import DivisionBar from "@/src/components/ui/DivisorBar";
 
 export default function CheckoutProduct() {
-  const [isConfirmDeletionModalOpen, setIsConfirmDeletionModalOpen] = useState(false); 
+  const [isConfirmDeletionModalOpen, setIsConfirmDeletionModalOpen] =
+    useState(false);
+
   const movie = useCheckoutStore((state) => state.movie);
   const seats = useCheckoutStore((state) => state.seats);
   const session = useCheckoutStore((state) => state.session);
   const tickets = useCheckoutStore((state) => state.tickets);
   const lumibar = useCheckoutStore((state) => state.lumibar);
   const clearCheckout = useCheckoutStore((state) => state.clearCheckout);
+
   const router = useRouter();
-  
+
   const hasSessionInfo =
-    session.language !== "" ||
-    session.date !== null ||
-    session.time !== "" 
+  !!session.language ||
+  !!session.date ||
+  !!session.time;
 
-  const hasSeatsInfo = seats.length > 0;
-
+  console.log("hasSeatsInfo", seats)
+  console.log(hasSessionInfo, "hasSessionInfo")
+  console.log("haslumibar:", lumibar)
 
   function calcItemsQuantity() {
     const ticketsSum = tickets.full.quantity + tickets.half.quantity;
@@ -36,21 +41,20 @@ export default function CheckoutProduct() {
   }
 
   return (
-  <>
-    <div
-      className="
-        w-full
-        lg:flex-1
-        bg-secondary-dark
-        p-4
-        rounded-xl
-        flex
-        flex-col
-        h-fit
-      "
-    >
-      <section className="flex flex-col gap-3 mb-3">
-        <div className="flex justify-between">
+    <>
+      <div
+        className="
+          w-full
+          lg:flex-1
+          bg-secondary-dark
+          p-4
+          rounded-xl
+          flex
+          flex-col
+          h-fit
+        "
+      >
+        <div className="flex justify-between mb-2">
           <h1>Resumo do Pedido</h1>
 
           <Trash2
@@ -59,137 +63,178 @@ export default function CheckoutProduct() {
             className="hover:stroke-red-500 cursor-pointer transition"
           />
         </div>
+        {movie && (
+          <>
+            <DivisionBar />
 
-        <div className="flex flex-col justify-center items-center gap-3">
-          <img
-            className="w-27 rounded"
-            src={movie?.poster_url}
-            alt={movie?.title}
-          />
+            <section className="flex flex-col gap-3 my-3">
+              <div className="flex flex-col justify-center items-center gap-3">
+                <img
+                  className="w-27 rounded"
+                  src={movie?.poster_url}
+                  alt={movie?.title}
+                />
 
-          <h2 className="text-font-dark text-[19px]">
-            {movie?.title}
-          </h2>
-        </div>
-      </section>
+                <h2 className="text-font-dark text-[19px]">
+                  {movie?.title}
+                </h2>
+              </div>
+            </section>
+          </>
+        )}
 
-      {hasSessionInfo && (
-        <>
+        {hasSessionInfo && (
+          <>
+            <DivisionBar />
+
+            <section className="my-5">
+              {session.language && (
+                <div className="flex justify-between">
+                  <p className="text-font-secondary-dark">Idioma</p>
+                  <p className="text-font-dark">{session.language}</p>
+                </div>
+              )}
+
+              {session.date && (
+                <div className="flex justify-between mt-3">
+                  <p className="text-font-secondary-dark">Data</p>
+                  <p className="text-font-dark">{session.date}</p>
+                </div>
+              )}
+
+              {session.time && (
+                <div className="flex justify-between mt-3">
+                  <p className="text-font-secondary-dark">Horário</p>
+                  <p className="text-font-dark">{session.time}</p>
+                </div>
+              )}
+               {hasSessionInfo && seats.length > 0 && (
+                <>
+
+                  <section className="flex justify-between my-4">
+                    <p className="text-font-secondary-dark">Assentos</p>
+
+                    <div className="flex justify-end gap-2">
+                      {seats.map((seat, index) => (
+                        <p key={seat} className="text-font-dark">
+                          {seat}
+                          {index < seats.length - 1 && ","}
+                        </p>
+                      ))}
+                    </div>
+                  </section>
+                </>
+              )}
+            </section>
+            
+          </>
+        )}
+
+        {lumibar.length > 0 && (
+          <>
+            <DivisionBar />
+
+            <div className="flex flex-col items-center mt-4">
+              <p className="text-font-secondary-dark mb-3">
+                Pedidos Lumibar
+              </p>
+
+              <div
+                className={`lumibar-scroll bg-[#262626] rounded w-full overflow-y-auto flex flex-col gap-2 p-3 items-start ${
+                  hasSessionInfo ? "h-30" : "h-full"
+                }`}
+              >
+                {lumibar.map((item, index) => (
+                  <div key={index} className="flex justify-between w-full">
+                    <div className="flex gap-2">
+                      <span className="text-[#AAA9A8]">
+                        x{item.quantity}
+                      </span>
+
+                      <p className="text-font-dark">{item.name}</p>
+                    </div>
+
+                    <p className="text-font-secondary-dark">
+                      R$ {item.price * item.quantity},00
+                    </p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </>
+        )}
+
+        <div className="mt-4">
           <DivisionBar />
 
-          <section className="mt-5 mb-5">
-            {session.language && (
-              <div className="flex justify-between">
-                <p className="text-font-secondary-dark">Idioma</p>
-                <p  className="text-font-dark">{session.language}</p>
-              </div>
-            )}
+          <div className="pt-4">
+            <div className="flex justify-between">
+              <p className="text-font-secondary-dark">Itens</p>
 
-            {session.date && (
-              <div className="flex justify-between mt-3">
-                <p className="text-font-secondary-dark">Data</p>
-                <p className="text-font-dark">{session.date}</p>
-              </div>
-            )}
-
-            {session.time && (
-              <div className="flex justify-between mt-3">
-                <p className="text-font-secondary-dark">Horário</p>
-                <p className="text-font-dark">{session.time}</p>
-              </div>
-            )}
-          </section>
-        </>
-      )}
-      {hasSeatsInfo && (
-        <section className="flex justify-between">
-          <p className="text-font-secondary-dark">Assentos</p>
-           <div className="flex justify-end gap-2">
-              {seats.map((seat, index) => (
-                <p key={seat} className="text-font-dark">
-                  {seat}
-                  {index < seats.length - 1 && ","}
+              <div className="w-10">
+                <p className="text-font-secondary-dark">
+                  {calcItemsQuantity()}
                 </p>
-              ))}
+              </div>
             </div>
-        </section>
-      )}
-      {lumibar.length > 0 && (
-        <>
-          <div className="flex justify-center items-center flex-col mt-4">
-             <DivisionBar />
-            <p className="text-font-secondary-dark mt-3">Pedidos Lumibar</p>
-            <div  className="lumibar-scroll bg-[#262626] rounded w-full h-30 overflow-y-auto flex flex-col gap-2 p-3 mb-3 items-start ">
-              {lumibar.map((item, index) => (
-                <div key={index} className="flex gap-2">
-                  <span className="text-[#AAA9A8]">x{item.quantity}</span>
-                  <p className="text-font-dark">{item.name}</p>
-                </div>
-              ))}
+
+            <div className="flex justify-between mt-3">
+              <p className="text-font-dark font-medium">Total</p>
+
+              <p className="text-font-dark font-medium">
+                R$ {BRFormat(calcItemsTotalPrice(tickets, lumibar))}
+              </p>
             </div>
-          </div>
-        </>
-      )}
-      
-      <div className="mt-4">
-        <DivisionBar />
-
-        <div className="pt-4">
-          <div className="flex justify-between">
-            <p className="text-font-secondary-dark">Itens</p>
-
-            <div className="w-10">
-              <p className="text-font-secondary-dark">{calcItemsQuantity()}</p>
-            </div>
-          </div>
-
-          <div className="flex justify-between mt-3">
-            <p  className="text-font-dark font-medium">Total</p>
-            <p  className="text-font-dark font-medium">R$ {BRFormat(calcItemsTotalPrice(tickets, lumibar))}</p>
           </div>
         </div>
       </div>
-    </div>
 
-    {isConfirmDeletionModalOpen && (
-      <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
-        <div className="w-115 rounded-2xl bg-secondary-dark shadow-2xl p-6">
-          <h2 className="text-xl font-semibold text-font-dark">
-             Cancelar Pedido
-          </h2>
+      {isConfirmDeletionModalOpen && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
+          <div className="w-115 rounded-2xl bg-secondary-dark shadow-2xl p-6">
+            <h2 className="text-xl font-semibold text-font-dark">
+              Cancelar Pedido
+            </h2>
 
-          <p className="mt-3 text-font-secondary-dark">
-            Ao confirmar, seu pedido será cancelado e você será redirecionado para a página inicial.
-          </p>
+            <p className="mt-3 text-font-secondary-dark">
+              Ao confirmar, seu pedido será cancelado e você será redirecionado
+              para a página inicial.
+            </p>
 
-          <div className="mt-8 flex justify-end gap-3">
-            <ButtonCine
-              text="Cancelar"
-              className="
-                bg-tertiary-dark w-32 cursor-pointer
-                transition-all duration-200
-                hover:brightness-110
-              "
-              onClick={() => setIsConfirmDeletionModalOpen(false)}
-            />
-
-            <ButtonCine
-              text="Confirmar"
-              className="
-                  h-12
-                  bg-accent w-32 cursor-pointer
-                  transition-all duration-200
+            <div className="mt-8 flex justify-end gap-3">
+              <ButtonCine
+                text="Cancelar"
+                className="
+                  bg-tertiary-dark
+                  w-32
+                  cursor-pointer
+                  transition-all
+                  duration-200
                   hover:brightness-110
-              "
-              onClick={() => {
-                clearCheckout();
-                router.push("/");
-              }}
-            />
+                "
+                onClick={() => setIsConfirmDeletionModalOpen(false)}
+              />
+
+              <ButtonCine
+                text="Confirmar"
+                className="
+                  h-12
+                  bg-accent
+                  w-32
+                  cursor-pointer
+                  transition-all
+                  duration-200
+                  hover:brightness-110
+                "
+                onClick={() => {
+                  clearCheckout();
+                  router.push("/");
+                }}
+              />
+            </div>
           </div>
         </div>
-      </div>
-    )}
-  </>
-);
+      )}
+    </>
+  );
 }
